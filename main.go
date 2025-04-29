dd234c9527788ca8ced857e9b59534f7f99a76f0
David Ochiel <nguonodave@gmail.com>
2025-04-29 21:08:30 +0300

    feat: basic ws go server

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// upgrader upgrades http conns to websocket conns
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // allow all domain conns
+	},
+}
+
+// handleWebSocket uses the upgrader to upgrade the http conn
+// then reads and writes messages from and to the ws
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// upgrade initial get request to a WebSocket
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("WebSocket upgrade failed:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Client connected via WebSocket!")
+
+	for {
+		// read message from client
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Read error:", err)
+			break
+		}
+		fmt.Println("Received message:", string(msg))
+	}
+}
+
+func main() {
+	// serve static files
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	// WebSocket endpoint
+	http.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
